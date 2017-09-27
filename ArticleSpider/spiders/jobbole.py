527fee4472f7435a959d7cc58402e634cb1c2be4
yuanoung <yuanoung@gmail.com>
2017-09-27 21:02:26 +0800

    item的设计

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -3,6 +3,8 @@ import scrapy
 import re
 from scrapy.http import Request
 from urllib import parse
+from ArticleSpider.items import JobBoleArticleItem
+from ArticleSpider.utils.common import get_md5
 
 
 class JobboleSpider(scrapy.Spider):
@@ -17,17 +19,22 @@ class JobboleSpider(scrapy.Spider):
         """
 
         # 第一步
-        post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
-        for post_url in post_urls:
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url}, callback=self.parse_detail)
 
         # 提取下一页的url
-            next_urls = response.css(".next.page-numbers::attr(href)").extract_first("")
-            if next_urls:
-                yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
+        next_urls = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_urls:
+            yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
 
     def parse_detail(self, response):
         # 第二步
+        article_item = JobBoleArticleItem()
+
+        front_image_url = response.meta.get("front_image_url", "")  # 文章封面图
         title = response.xpath('//div[@class="entry-header"]//h1/text()').extract_first()
         create_date = response.xpath(
             '//p[@class="entry-meta-hide-on-mobile"]/text()').extract_first().strip().replace("·", "").strip()
@@ -48,5 +55,19 @@ class JobboleSpider(scrapy.Spider):
 
         content = response.xpath("//div[@class='entry']").extract_first()
         tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
-        tag_list = ", ".join([element for element in tag_list if not element.strip().endswith("评论")])
-        pass
+        tags = ", ".join([element for element in tag_list if not element.strip().endswith("评论")])
+
+        article_item.update({
+            "title": title,
+            "url": response.url,
+            "url_object_id": get_md5(response.rul),
+            "create_date": create_date,
+            "front_image_url": [front_image_url],
+            "praise_nums": praise_nums,
+            "comment_nums": comment_nums,
+            "fav_nums": fav_nums,
+            "tags": tags,
+            "content": content,
+        })
+
+        yield article_item
